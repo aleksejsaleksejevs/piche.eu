1e8ad396183c925dd4fd1f4e73dbf53e5e5c791c
aleksejs aleksjevs <aleksejs.aleksjevs@gmail.com>
2015-02-25 23:13:26 +0200

    first

---
+++ js/additional.js
@@ -0,0 +1,5 @@
+(function ($, Drupal, window, document, undefined)
+  $(window).ready(function() {
+    $(".form-item-field-burgera-tips-tid").addClass('tvnet');
+  });
+})(jQuery, Drupal, this, this.document);
